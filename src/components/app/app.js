21b9dacbd1db15d594fb7f90b7fc667426c6ee47
Wesley Nguyen <nguyenw@seattleu.edu>
2020-10-05 20:33:42 -0700

    restructure files

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="app_background">
+        <h1> Testing </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
