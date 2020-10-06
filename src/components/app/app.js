ef68b57d3b51ca74b6d5b41ce60151c66804e357
Wesley Nguyen <nguyenw@seattleu.edu>
2020-10-06 15:07:06 -0700

    finish auth loading

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import './app.css';
 
-function App() {
-  return (
+class App extends React.Component {
+
+  // fake authentication Promise
+  // Set up new auth later for app
+  authenticate(){
+    return new Promise(resolve => setTimeout(resolve, 2000)) // 2 seconds
+  }
+
+  componentDidMount(){
+    this.authenticate().then(() => {
+      const ele = document.getElementById('ipl-progress-indicator')
+      if(ele){
+        // fade out
+        ele.classList.add('available')
+        setTimeout(() => {
+          // remove from DOM
+          ele.outerHTML = ''
+        }, 2000)
+      }
+    })
+  }
+
+  render() {
+    return (
     <div className="App">
       <div id="app_background">
-        <h1> Testing </h1>
+        <h1> yall doodoo </h1>
       </div>
     </div>
   );
+  }
 }
 
 export default App;
